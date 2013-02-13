1b196366b66948f66de96f4b6b8cffc8201431e6
Shaun + Sarah <shaun+sarah@appacademy.io>
2013-02-12 18:03:24 -0800

    up through fibonacci recursive

--- exercises.js
+++ exercises.js
@@ -28,7 +28,7 @@ module.exports = {
         };
       };
     };
-  }, 
+  },
 
   transpose: function(grid) {
     var newGrid = [];
@@ -44,7 +44,90 @@ module.exports = {
     };
 
     console.log(newGrid);
+  },
+
+  doubler: function(array) {
+    var newArr = [];
+    array.map( function(num) { newArr.push(num * 2) } );
+
+    console.log(newArr);
+  },
+
+  myEach: function(array, f) {
+    array.map( function(num) { f(num); });
+  },
+
+  inject: function(array, start, f) {
+    var result = start;
+
+    array.map( function(num) {
+      holder = f(result, num);
+      result = holder;
+    });
+    console.log(result);
+  },
+
+  bubbleSort: function(array) {
+    var sorted = false;
+
+    while (sorted == false) {
+      sorted = true;
+      for (var i = 0; i < array.length - 1; i++) {
+        if (array[i] > array[i + 1]) {
+          temp = array[i];
+          array[i] = array[i + 1];
+          array[i + 1] = temp;
+
+          sorted = false;
+        };
+      };
+    };
+    console.log(array);
+  },
+
+  substrings: function(string) {
+    var substrings = [];
+
+    for (var i = 0; i < string.length; i++) {
+      for (var j = i + 1; j <= string.length; j++) {
+        substrings.push(string.substring(i, j));
+      };
+
+    };
+
+    console.log(substrings);
+  },
+
+  recSumArray: function(array) {
+    if (array.length == 0) {
+      return 0;
+    } else if (array.length == 1) {
+      return array[0];
+    } else {
+      return (array.pop()+ this.recSumArray(array));
+    };
+  },
+
+  exp1: function(b, n) {
+    if (n == 0) {
+      return 1;
+    } else {
+      return ( b * this.exp1(b, n - 1));
+    };
+  },
+
+  fibRec: function(n) {
+    if (n == 1) {
+      return [0];
+    } else if (n == 2) {
+      return [0,1];
+    } else {
+      var oneLess = this.fibRec(n-1);
+      oneLess.push(oneLess[oneLess.length-1] + oneLess[oneLess.length-2]);
+      return oneLess;
+    };
   }
+
 
 };
 
--- test.js
+++ test.js
@@ -11,3 +11,17 @@ var DemoCode = require('./exercises');
 // DemoCode.transpose([[1,2],[3,4]]);
 // console.log('transpose2');
 // DemoCode.transpose([[1,2,3],[4,5,6],[7,8,9]]);
+// DemoCode.doubler([1,4,7]);
+// DemoCode.myEach([1, 2, 3], function (element) { console.log(element) });
+// DemoCode.inject([1,2,3], 0, function(sum, number) { return sum + number; });
+// DemoCode.bubbleSort([5, 1, 3, 7, 1, 1, 9, 6, 3]);
+// DemoCode.substrings("marryChristMas");
+
+// var test = DemoCode.recSumArray([5,3,1]);
+// console.log(test);
+
+// var test = DemoCode.exp1(2, 4);
+// console.log(test);
+
+var test = DemoCode.fibRec(6);
+console.log(test);
